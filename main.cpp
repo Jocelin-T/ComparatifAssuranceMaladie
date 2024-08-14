7f9ddb80d0398f446d64373cf0e8853a82c5d4e1
Jocelin Thumelin <jocelin.thumelin@formation.orif.ch>
2024-08-14 14:42:30 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+// Compart la primes des assurances maladie
+// 
+//
+// *** La franchise *** //
+// 
+// La franchise est le montant que vous devez payer vous-même chaque
+//	année à titre de participation aux coûts.Ce n’est qu’une fois ce
+//	montant atteint que votre caisse-maladie prendra le relai.
+// La franchise minimale des adultes est de 300 francs, la plus haute,
+//	de 2500 francs; pour les enfants, la franchise se situe entre 0 et 600 francs.
+// 
+// Un exemple : la franchise que vous avez choisie est de 300 francs et 
+//	vous devez vous rendre deux fois chez le médecin. Celui-ci facture 
+//	chaque visite 200 francs.Vous payez la première facture, plus 100 francs de la seconde;
+//	la caisse-maladie prend en charge les 100 francs restants – moins 10 francs de quote-part
+//	ainsi que toutes les autres factures de l’année en cours.
+
+
+// *** La quote-part *** //
+
+// Une fois la franchise atteinte, 
+//	vous ne payez plus qu’une quote-part de 10 % des frais,
+//	plafonnée à 700 francs par an(350 francs pour les enfants).
+// Votre participation maximale aux frais s’élève donc au montant de votre franchise, 
+//	auquel s’ajoutent 700 francs de quote-part.
+
+
+int main(){
+
+	std::cout << "Hello World\n";
+
+	return 0;
+}
